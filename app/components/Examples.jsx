502c0d7a30f176133b3b6cb3aee006ed9ee7d8b4
Jacob <jacobmoyle@gmail.com>
2017-05-17 17:30:17 -0700

    add foundation grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few locations to try out</p>
+      <ol>
+        <li>
+          <Link to="/?location=Reno">Reno, NV</Link>
+        </li>
+        <li>
+          <Link to="/?location=London">London</Link>
+        </li>
+      </ol>
+    </div>
   );
 }
 
